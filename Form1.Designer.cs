2787dbd04971c7b4b5348dfd01ec7178199f0587
Andres <Andres@HP>
2024-11-26 21:59:39 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace gato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FichasGato = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.picGanador = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picReiniciar = new System.Windows.Forms.PictureBox();
+            this.lblPlayer1 = new System.Windows.Forms.Label();
+            this.lblPlayer2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picReiniciar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FichasGato
+            // 
+            this.FichasGato.BackColor = System.Drawing.Color.Transparent;
+            this.FichasGato.ColumnCount = 3;
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.FichasGato.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
+            this.FichasGato.Location = new System.Drawing.Point(94, 145);
+            this.FichasGato.Name = "FichasGato";
+            this.FichasGato.RowCount = 3;
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.FichasGato.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 99F));
+            this.FichasGato.Size = new System.Drawing.Size(315, 315);
+            this.FichasGato.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::gato.Properties.Resources.f_1;
+            this.pictureBox1.Location = new System.Drawing.Point(554, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(730, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "VS";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::gato.Properties.Resources.f_2;
+            this.pictureBox2.Location = new System.Drawing.Point(795, 45);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(158, 131);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // picGanador
+            // 
+            this.picGanador.Image = global::gato.Properties.Resources.img_negro;
+            this.picGanador.Location = new System.Drawing.Point(1046, 45);
+            this.picGanador.Name = "picGanador";
+            this.picGanador.Size = new System.Drawing.Size(261, 233);
+            this.picGanador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGanador.TabIndex = 4;
+            this.picGanador.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1110, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "GANADOR";
+            // 
+            // picReiniciar
+            // 
+            this.picReiniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picReiniciar.Image = global::gato.Properties.Resources.reiniciar;
+            this.picReiniciar.Location = new System.Drawing.Point(125, 524);
+            this.picReiniciar.Name = "picReiniciar";
+            this.picReiniciar.Size = new System.Drawing.Size(257, 48);
+            this.picReiniciar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picReiniciar.TabIndex = 6;
+            this.picReiniciar.TabStop = false;
+            this.picReiniciar.Click += new System.EventHandler(this.picReiniciar_Click);
+            // 
+            // lblPlayer1
+            // 
+            this.lblPlayer1.AutoSize = true;
+            this.lblPlayer1.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer1.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer1.Location = new System.Drawing.Point(610, 196);
+            this.lblPlayer1.Name = "lblPlayer1";
+            this.lblPlayer1.Size = new System.Drawing.Size(25, 25);
+            this.lblPlayer1.TabIndex = 7;
+            this.lblPlayer1.Text = "0";
+            // 
+            // lblPlayer2
+            // 
+            this.lblPlayer2.AutoSize = true;
+            this.lblPlayer2.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer2.ForeColor = System.Drawing.Color.White;
+            this.lblPlayer2.Location = new System.Drawing.Point(856, 196);
+            this.lblPlayer2.Name = "lblPlayer2";
+            this.lblPlayer2.Size = new System.Drawing.Size(25, 25);
+            this.lblPlayer2.TabIndex = 8;
+            this.lblPlayer2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::gato.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(1371, 664);
+            this.Controls.Add(this.lblPlayer2);
+            this.Controls.Add(this.lblPlayer1);
+            this.Controls.Add(this.picReiniciar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picGanador);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.FichasGato);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGanador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picReiniciar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel FichasGato;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picGanador;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picReiniciar;
+        private System.Windows.Forms.Label lblPlayer1;
+        private System.Windows.Forms.Label lblPlayer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gato
+{
+    public partial class Form1 : Form
+    {
+        //se declaran las variables que se ocuparan
+        int turno = 1;
+        int[,] Gato;
+        int PuntosPlayer1 = 0;
+        int PuntosPlayer2 = 0;
+        bool YaHayGanador;
+        public Form1()
+        {
+            InitializeComponent();
+            IniciarJuego();
+        }
+        public void IniciarJuego()
+        {
+            //Iniciar valores en el juego
+            turno = 1;
+            Gato = new int[3, 3];
+            YaHayGanador = false;
+
+            picGanador.Image = Properties.Resources.img_negro;
+            FichasGato.Controls.Clear();
+
+            // Este arreglo es para mostrar las fichas en el tablero y meter los valores en una matriz
+            for (var i = 0; i<3; i++)
+            {
+                for (var j = 0; j<3; j++)
+                {
+                    var FichaJuego = new PictureBox();
+                    FichaJuego.Image = Properties.Resources.img_negro;
+                    FichaJuego.Name = string.Format("{0}", i + "_" + j);
+                    FichaJuego.Dock = DockStyle.Fill;
+                    FichaJuego.Cursor = Cursors.Hand;
+                    FichaJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    FichaJuego.Click += Jugar;
+
+                    FichasGato.Controls.Add(FichaJuego, j, i);
+                    Gato[i, j] = 0;
+                }
+            }
+        }
+
+        private void Jugar(object sender, EventArgs e)
+        {
+            var FichaSeleccionadaUsuario = (PictureBox)sender;
+            FichaSeleccionadaUsuario.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+            string[] Posicion = FichaSeleccionadaUsuario.Name.Split("_".ToCharArray());
+            int Fila = Convert.ToInt32(Posicion[0]);
+            int Columna = Convert.ToInt32(Posicion[1]);
+            Gato[Fila, Columna] = turno;
+            VerificarJuego(Fila, Columna);
+            turno = (turno == 1) ? 2 : 1;
+        }
+
+        public void VerificarJuego(int Fila, int Columna)
+        {
+            //contador y verificador de fichas del gato, para saber en que momento alguien gana
+            int GanoFilas = 0;
+            int GanoColumnas = 0;
+            int DiagonalPrincipal = 0;
+            int DiagonalInversa = 0;
+            int TamanioGato = 3;
+
+            for (var i = 0; i<TamanioGato; i++)
+            {
+                for (var j = 0; j < TamanioGato ; j++)
+                {
+                    if (i == Fila)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoFilas++;
+                        }
+                    }
+                    if (j == Columna)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            GanoColumnas++;
+                        }
+                    }
+                    if (i == j)
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalPrincipal++;
+                        }
+                    }
+                    if ((i + j) == (TamanioGato - 1))
+                    {
+                        if (Gato[i, j] == turno)
+                        {
+                            DiagonalInversa++;
+                        }
+                    }
+                }
+            }
+            if ((GanoFilas == TamanioGato) || (GanoColumnas == TamanioGato) || (DiagonalInversa == TamanioGato) || (DiagonalPrincipal == TamanioGato))
+            {
+                YaHayGanador = true;
+            }
+            if (YaHayGanador)
+            {
+                MessageBox.Show("ya hay un ganador");
+                picGanador.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("f_" + turno);
+                if (turno == 1)
+                {
+                    PuntosPlayer1++;
+                    lblPlayer1.Text = PuntosPlayer1.ToString();
+                }
+                else
+                {
+                    PuntosPlayer2++;
+                    lblPlayer2.Text = PuntosPlayer2.ToString();
+                }
+            }
+        }
+
+        private void picReiniciar_Click(object sender, EventArgs e)
+        {
+            IniciarJuego();
+        }
+    }
+}
